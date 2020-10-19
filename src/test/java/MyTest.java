0c7a3b3dee96ac85309498a28bc0d5f27cdb9e53
meetBOBpatel <59351766+meetBOBpatel@users.noreply.github.com>
2020-10-18 23:19:49 -0500

    Working test cases

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.DisplayName;
@@ -7,11 +8,43 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import javafx.scene.layout.GridPane;
+
 class MyTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	static JavaFXTemplate keno;
+	static int iter;
+
+	@BeforeAll
+	static void init() {
+		keno = new JavaFXTemplate();
+		iter = -1;
 	}
 
+	@ParameterizedTest
+	@ValueSource(ints = {0,0,1,11,27,75,420,1100,4500,10000,30000,100000})
+	void scoreTest(int score) {
+		
+		int scoreReturned = keno.ScoreCal(iter);
+		iter++; 
+		assertEquals(score, scoreReturned, "score returned is not the same as expected");
+	}
+	
+	@Test
+	void randomNumberTest() {
+		
+		int returnVal = keno.GetRandomNumber();
+		
+		assertTrue(returnVal >= 1 && returnVal <= 80);
+	}
+	
+	@Test
+	void randomNumber2Test() {
+		
+		int returnVal = keno.GetRandomNumber();
+		
+		assertFalse(returnVal <= 1 && returnVal >= 80);
+	}
+
+	
 }
